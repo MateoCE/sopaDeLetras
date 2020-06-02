bad0ec9c671fa4ae9e9d5a7183e4c7c3888c6af2
Mateo <mateo.nal@gmail.com>
2020-06-02 12:26:30 +0200

    estructura basica sopa de letras

--- main.php
+++ main.php
@@ -1,4 +1,5 @@
 <?php
+
     if(isset($_POST['nickName'])){
         $nickName= $_POST['nickName'];
     }else{
@@ -7,14 +8,37 @@
 
     if(isset($_POST['size'])){
         $size = $_POST['size'];
+        switch ($size) {
+            case 'small':
+                $width = 10;
+                $height= 10;
+                $numWords=5;
+                break;
+            case 'medium':
+                $width = 15;
+                $height= 15;
+                $numWords=7;
+                break;
+            case 'big':
+                $width = 20;
+                $height= 20;
+                $numWords=10;
+                break;
+            default:
+                $width = 15;
+                $height= 15;
+                $numWords=7;
+                break;
+        }
     }else{
         $size = "medium";
+        $width = 15;
+        $height= 15;
+        $numWords=7;
     }
 
-    echo $nickName;
-    echo $size;
 
-    //Leer archivo txt y guardar palabras en array.
+    //guardamos todas las palabras del txt en un array
     $file = fopen("listaPalabras", "r");
     $diccionario = [];
     $contador=0;
@@ -25,16 +49,78 @@
     }
     fclose($file);
 
-    //Seleccionamos 6 palabras aleatorias.
-    $palabrasRandom = [];
+    //seleccionamos las palabras que usaremos en la sopa de letras segun el tamaño seleccionado
+    $word = [];
     $contador=0;
-    while ($contador<6) {
+    while ($contador<$numWords) {
         $numAleatorio = rand(0,count($diccionario)-2);
-        if (!in_array($diccionario[$numAleatorio], $palabrasRandom)) {
-            array_push($palabrasRandom, $diccionario[$numAleatorio]);
+        if (!in_array($diccionario[$numAleatorio], $word)) {
+            array_push($word, $diccionario[$numAleatorio]);
             $contador++;
         }
     }
 
-    print_r($palabrasRandom);
+    //juntamos todos los datos necesarion en un array
+    $datos = array(
+        width => $width,
+        height => $height,
+        name => $nickName,
+        numWords => $numWords
+    );
+
+    //creamos un tablero con un . en cada casilla
+    function crearArrayTabla(){
+        global $tabla, $datos;
+        for ($row = 0; $row < $datos["height"]; $row++){
+            for ($col = 0; $col < $datos["width"]; $col++){
+              $tabla[$row][$col] = ".";
+            }
+          } 
+    }
+
+    //creamos la cuadricula con los datos que contega el array "board" y la guardamos en una variable
+    function crearTabla($tabla){
+        global $datos;
+        $sopaDeLetras = "";
+        $sopaDeLetras .= "<table border = 0>\n";
+        for ($row = 0; $row < $datos["height"]; $row++){
+          $sopaDeLetras .= "<tr>\n";
+          for ($col = 0; $col < $datos["width"]; $col++){
+            $sopaDeLetras .= " <td width = 15>{$tabla[$row][$col]}</td>\n";
+          }
+          $sopaDeLetras .= "</tr>\n";
+        }
+        $sopaDeLetras .= "</table>\n";
+        return $sopaDeLetras;
+    }
+
+    function printSopaDeLetras(){
+      
+        global $sopaDeLetras, $word, $datos;
+        //print puzzle itself
+      
+        print <<<HERE
+        <center>
+        <h1>{$datos["name"]}</h1>
+        $sopaDeLetras
+        <h3>Word List</h3>
+        <table border = 0>
+      
+      HERE;
+      
+        //print word list
+        foreach ($word as $theWord){
+          print "<tr><td>$theWord</td></tr>\n";
+        } // end foreach
+        print "</table>\n";
+        //$puzzleName = $boardData["name"];
+
+      
+      } // end printPuzzle
+
+      crearArrayTabla();
+      $sopaDeLetras = crearTabla($tabla);
+      printSopaDeLetras();
+
+
 ?>
